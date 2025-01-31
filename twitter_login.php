a7a57388a8dde4345180a1c8e1a20bd6ed1b97f7
Doger Token <80608747+dogertoken@users.noreply.github.com>
2025-01-31 14:23:36 +0700

    Delete twitter_login.php

--- twitter_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-$client_id = "UzZYOEJsQ1U2MG9xeDlJb2FnOUE6MTpjaQ";
-$redirect_uri = "https://doger.my/twitter_callback.php";
-
-header("Location: https://twitter.com/i/oauth2/authorize?response_type=code&client_id=$client_id&redirect_uri=$redirect_uri&scope=tweet.read%20users.read%20follows.read%20offline.access&state=random_string&code_challenge=challenge&code_challenge_method=plain");
-exit;
