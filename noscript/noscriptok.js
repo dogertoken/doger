b20c17c29c4e64ab983e7215b0c31e4656d7899a
Doger Token <80608747+dogertoken@users.noreply.github.com>
2025-02-03 17:13:22 +0700

    Update noscriptok.js

--- noscript/noscriptok.js
+++ noscript/noscriptok.js
@@ -1,29 +1,20 @@
-        const maxSupply = 10000000000; // 10 Miliar Token
-
-// Gunakan RPC Resmi dari OPNB Testnet
-const rpcURL = "https://opbnb-testnet-rpc.bnbchain.org";
+const maxSupply = 10000000000;
+const rpcURL = "https://mainnet.base.org";
 const web3 = new Web3(new Web3.providers.HttpProvider(rpcURL));
-
-// Ganti dengan alamat kontrak token Anda
-const contractAddress = "0xf70C807DEE3bc21eb7732919Edcca4d290083474";
-
-// ABI Minimum untuk membaca total supply & minted supply
+const contractAddress = "0x6cB08635073dC88921c2819DAEb6A70e63abc91c";
 const contractABI = [{"inputs":[{"internalType":"address payable","name":"_taxCollector","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"EtherWithdrawn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"TokenBurned","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"taxPaid","type":"uint256"}],"name":"TokenMinted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"TokensWithdrawn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"BURN_PERCENTAGE","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"NET_TAX_PER_100000","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"TOKENS_PER_MINT","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address payable","name":"newTaxCollector","type":"address"}],"name":"changeTaxCollector","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"mintToken","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"taxCollector","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawEther","outputs":[],"stateMutability":"nonpayable","type":"function"}];
-// Inisialisasi kontrak
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-// Inisialisasi Chart.js dengan efek gradient 3D
 const ctx = document.getElementById('donutChart').getContext('2d');
 let donutChart;
 
 function createGradient(ctx) {
     let gradient = ctx.createLinearGradient(0, 0, 0, 400);
-    gradient.addColorStop(0, "#FFA500"); // Orange Atas
-    gradient.addColorStop(1, "#FF4500"); // Red Orange Bawah
+    gradient.addColorStop(0, "#FFA500");
+    gradient.addColorStop(1, "#FF4500");
     return gradient;
 }
 
-// Membuat chart pertama kali
 function createChart(mintedPercentage) {
     donutChart = new Chart(ctx, {
         type: 'doughnut',
@@ -46,7 +37,6 @@ function createChart(mintedPercentage) {
     });
 }
 
-// Update Chart saat ada perubahan minting
 async function updateChart() {
     try {
         const mintedSupply = await contract.methods.mintedSupply().call();
@@ -59,13 +49,11 @@ async function updateChart() {
             donutChart.update();
         }
     } catch (error) {
-        console.error("Gagal mengambil data minting:", error);
+        console.error("Failed to retrieve minting data:", error);
     }
 }
 
-// Jalankan fungsi pertama kali
 createChart(0);
 updateChart();
 
-// Refresh data setiap 10 detik
 setInterval(updateChart, 10000);
